8ecc8d18218f5f5d1a7506030f767275849cfb7c
Faraz Patankar <farazpatankar@gmail.com>
2022-07-04 13:37:50 +0400

    improve dropzone component + alert on reject

--- src/components/Dropzone.tsx
+++ src/components/Dropzone.tsx
@@ -1,16 +1,28 @@
-import { Text } from "@mantine/core";
+import { Center, createStyles, Stack, Text, Title } from "@mantine/core";
 import { Dropzone as MantineDropzone, MIME_TYPES } from "@mantine/dropzone";
+import { message } from "@tauri-apps/api/dialog";
+import { ImageIcon } from "./Icons";
 
-/**
- * onReject is currently broken on React 18, please
- * update this component once it is fixed to handle
- * rejection beautifully.
- */
+const useStyles = createStyles(theme => {
+  return {
+    container: {
+      color: theme.colors.pink[6],
+    },
+    title: {
+      color: theme.colors.pink[6],
+    },
+    text: {
+      color: theme.colors.gray[5],
+    },
+  };
+});
 
 interface DropzoneProps {
   setImage: React.Dispatch<React.SetStateAction<string | null>>;
 }
 export const Dropzone: React.FC<DropzoneProps> = ({ setImage }) => {
+  const { classes } = useStyles();
+
   return (
     <MantineDropzone
       accept={[MIME_TYPES.jpeg, MIME_TYPES.png, MIME_TYPES.webp]}
@@ -20,8 +32,24 @@ export const Dropzone: React.FC<DropzoneProps> = ({ setImage }) => {
 
         setImage(obj);
       }}
+      onReject={async rejectedFiles => {
+        const [rejectedFile] = rejectedFiles;
+        await message(rejectedFile.errors[0].message, {
+          title: rejectedFile.errors[0].code,
+          type: "error",
+        });
+      }}
+      multiple={false}
     >
-      {status => <Text>Images go here</Text>}
+      <MantineDropzone.Idle>
+        <Stack align="center" spacing="xs" className={classes.container}>
+          <ImageIcon />
+          <Title order={3} align="center" className={classes.title}>
+            Drag and drop or click here to select files
+          </Title>
+          <Text className={classes.text}>Images go here</Text>
+        </Stack>
+      </MantineDropzone.Idle>
     </MantineDropzone>
   );
 };
